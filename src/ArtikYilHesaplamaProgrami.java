334960f0eee02dc5539f5c03ad813ee94c2b0cbf
kerimaykutsengur <kaykut23@gmail.com>
2022-06-02 18:26:25 +0300

    Initial commit

---
+++ src/ArtikYilHesaplamaProgrami.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class ArtikYilHesaplamaProgrami {
+    public static void main(String[] args) {
+        int year;
+        int kalan;
+        int dortYuz;
+        int yuz;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Yılı Giriniz: ");
+        year = input.nextInt();
+
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    System.out.print(year + "artık bir yıldır!");
+                } else {
+                    System.out.print(year + "artık bir yıl değildir!");
+                }
+            } else {
+                System.out.print(year + "artık bir yıldır!");
+            }
+        } else {
+            System.out.print(year + "artık bir yıl değildir!");
+        }
+
+
+    }
+}
